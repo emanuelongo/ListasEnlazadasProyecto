b63b3125e71bba171e0d30135f7c309eb49f1858
emanuelongo <emanuel1001cano@gmail.com>
2024-04-08 18:25:34 -0500

    Changes

--- main.py/Libro.py
+++ main.py/Libro.py
@@ -1,2 +1,11 @@
-for _ in range(10):
-    print("Hello")
+class Libro:
+    
+    def __init__(self, numeroLibro: str, genero: str, autor: str, titulo: str, añoPublicacion: int, tarifaAlquiler):
+        self.numeroLibro = numeroLibro
+        self.genero = genero
+        self.autor = autor
+        self.titulo = titulo
+        self.añoPublicacion = añoPublicacion
+        self.tarifaAlquiler = tarifaAlquiler
+    
+
